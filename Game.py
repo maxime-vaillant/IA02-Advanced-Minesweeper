d800d1e6bf2056a414848b5ef9a2d74ca6c82c2d
“Maxime <max.vaillant1@orange.fr>
2021-06-10 09:12:33 +0200

    Change unique fonction into exact function with param (unique = exact with param 1) Add function to add new information about near cells

--- Game.py
+++ Game.py
@@ -1,6 +1,6 @@
 import subprocess
 from sys import platform
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 import itertools
 
 # U: Unknown, F: Free, T: Tiger, C: Crocodile, S: Shark, W: Water
@@ -85,6 +85,23 @@ class Game:
         with open(self.file, "w", newline="") as cnf:
             cnf.write(dimacs)
 
+    @staticmethod
+    def clauses_to_dimacs(clauses: List[List[int]], nb_vars: int) -> str:
+        """
+        Change clauses to their dimacs value
+        :param clauses: List of clauses
+        :param nb_vars: number vars in the dimacs
+        :return: dimacs value
+        """
+        end = "0\n"
+        space = " "
+        dimacs = "p cnf " + str(nb_vars) + space + str(len(clauses)) + "\n"
+        for clause in clauses:
+            for atom in clause:
+                dimacs += str(atom) + space
+            dimacs += end
+        return dimacs
+
     def cell_to_variable(self, i: int, j: int, val: str) -> int:
         """
         Transform a cell representation to a variable
@@ -103,29 +120,37 @@ class Game:
         """
         i, rest = var // (self.width * length), var % (self.width * length)
         j, rest = rest // length, rest % length
-        val = values_list[rest-1]
+        val = values_list[rest - 1]
         return i, j, val
 
     def at_least_one(self, vars: List[int]) -> List[int]:
         return vars[:]
 
-    def unique(self, vars: List[int]) -> List[List[int]]:
+    def exact(self, vars: List[int], param: int) -> List[List[int]]:
         """
         Take a list of clauses and remove all duplicates ones
+        :param param: number exact
         :param vars: in clauses list
         :return: list of unique clauses
         """
-        clauses = [self.at_least_one(vars)]
-        for clause in itertools.combinations([-x for x in vars[:]], 2):
-            i, j = clause
-            clauses.append([i, j])
+        clauses = [self.at_least_one(vars)] if param != 0 else []
+        for combination in itertools.combinations([-x for x in vars[:]], param+1):
+            clause = []
+            for i in range(param+1):
+                clause.append(combination[i])
+            clauses.append(clause)
         return clauses
+
+    def getNearCells(self, i: int, j: int) -> List[List[int]]:
+        cells = []
+        for a in range(i - 1, i + 2):
+            for b in range(j - 1, j + 2):
+                if 0 <= a <= self.height and 0 <= b <= self.width and (a != i or b != j):
+                    cells.append([a, b])
+        return cells
 
     # Création des règles liées aux animaux
     # On applique les règles pour toutes les cases du démineur
-    # unique: U v F v C v S v T
-    # -T v -W
-    # -S v W
     def create_animals_constraints(self) -> List[List[int]]:
         clauses = []
         for i in range(self.height):
@@ -140,7 +165,31 @@ class Game:
                         clauses.append([-self.cell_to_variable(i, j, key), -self.cell_to_variable(i, j, "W")])
                     elif key == "S":
                         clauses.append([-self.cell_to_variable(i, j, key), self.cell_to_variable(i, j, "W")])
-                clauses += self.unique(cells)
+                clauses += self.exact(cells, 1)
+        return clauses
+
+    def add_information_constraints(self, data: Dict) -> List[List[int]]:
+        clauses = []
+        pos = data["pos"]
+        field = data["field"]
+        f = self.cell_to_variable(pos[0], pos[1], "W") if field == "sea" else -self.cell_to_variable(pos[0], pos[1], "W")
+        proximity_count = data.get("prox_count", None)
+        if proximity_count:
+            near_cells = self.getNearCells(pos[0], pos[1])
+            animals = ("T", "S", "C")
+            total_count = 0
+            for index, count in enumerate(proximity_count):
+                total_count += count
+                cells = []
+                animal = animals[index]
+                for cell in near_cells:
+                    cells.append(self.cell_to_variable(cell[0], cell[1], animal))
+                clauses += self.exact(cells, count)
+            cells = []
+            for cell in near_cells:
+                cells.append(self.cell_to_variable(cell[0], cell[1], "F"))
+            clauses += self.exact(cells, len(near_cells) - total_count)
+        clauses.append([f])
         return clauses
 
     def create_game_constraints(self):
@@ -154,19 +203,3 @@ class Game:
         # TODO create clauses
 
         self.write_dimacs_file(self.clauses_to_dimacs(clauses, length * self.width * self.height))
-
-    def clauses_to_dimacs(self, clauses: List[List[int]], nb_vars: int) -> str:
-        """
-        Change clauses to their dimacs value
-        :param clauses: List of clauses
-        :param nb_vars: number vars in the dimacs
-        :return: dimacs value
-        """
-        end = "0\n"
-        space = " "
-        dimacs = "p cnf " + str(nb_vars) + space + str(len(clauses)) + "\n"
-        for clause in clauses:
-            for atom in clause:
-                dimacs += str(atom) + space
-            dimacs += end
-        return dimacs
