880beb062079065ff9f30d64e7a3752b23b864e1
“Maxime <max.vaillant1@orange.fr>
2021-06-11 17:19:49 +0200

    V1 mine solver

--- Game.py
+++ Game.py
@@ -1,5 +1,4 @@
 import subprocess
-from pprint import pprint
 from sys import platform
 from typing import List, Tuple, Dict
 import itertools
@@ -7,12 +6,12 @@ import itertools
 # U: Unknown, F: Free, T: Tiger, C: Crocodile, S: Shark, W: Water
 values_list = ["L", "W", "F", "T", "C", "S"]
 values_dict = {
-    "L": 0,
-    "W": 1,
-    "F": 2,
-    "T": 3,
-    "C": 4,
-    "S": 5,
+    "L": 1,
+    "W": 2,
+    "F": 3,
+    "T": 4,
+    "C": 5,
+    "S": 6,
 }
 length = len(values_dict)
 
@@ -22,12 +21,13 @@ class Game:
     This class represents a game board
     """
 
-    def __init__(self, height: int, width: int, tiger_count: int, crocodile_count: int, shark_count: int, land_count: int, sea_count: int,  filename: str = "default.cnf"):
+    def __init__(self, height: int, width: int, tiger_count: int, crocodile_count: int, shark_count: int,
+                 land_count: int, sea_count: int, filename: str = "default.cnf"):
         """
         Default constructor
         :param filename: name of the cnf file
         """
-        self.board = [[["?", "?", []] for j in range(width)] for i in range(height)]
+        self.board = [[['?', '?', []] for j in range(width)] for i in range(height)]
         self.file = filename
         self.cmd = None
         self.width = width
@@ -46,13 +46,26 @@ class Game:
         elif platform == 'linux':
             self.cmd = "./gophersat-1.1.6-Linux"
 
-    def make_decision(self):
+    def make_decision(self) -> Tuple[str, Tuple]:
         self.write_dimacs_file(self.clauses_to_dimacs(self.clauses, self.height * self.width * length))
         response = self.exec_gophersat()
+        free = []
+        guess = []
         if response[0]:
             for var in response[1]:
                 if var > 0:
                     cell = self.variable_to_cell(var)
+                    if self.board[cell[0]][cell[1]][1] == '?' and cell[2] != 'W' and cell[2] != 'L':
+                        if cell[2] == 'F':
+                            free.append(cell)
+                        else:
+                            guess.append(cell)
+        if len(free) > 0:
+            return 'discover', free[0]
+        elif len(guess) > 0:
+            return 'guess', guess[0]
+        else:
+            return 'none', ()
 
     def exec_gophersat(self, encoding: str = "utf8") -> Tuple[bool, List[int]]:
         """
@@ -118,12 +131,14 @@ class Game:
         :param var: variable
         :return cell
         """
+        var -= 1
         i, rest = var // (self.width * length), var % (self.width * length)
-        j, rest = rest // length, rest % length
-        val = values_list[rest]
+        j = rest // length
+        val = values_list[(var + 1) % length - 1]
         return i, j, val
 
-    def at_least_one(self, vars: List[int]) -> List[int]:
+    @staticmethod
+    def at_least_one(vars: List[int]) -> List[int]:
         return vars[:]
 
     def exact(self, vars: List[int], param: int) -> List[List[int]]:
@@ -133,10 +148,11 @@ class Game:
         :param vars: in clauses list
         :return: list of unique clauses
         """
-        clauses = [self.at_least_one(vars)] if param != 0 else []
-        for combination in itertools.combinations([-x for x in vars[:]], param+1):
+        clauses = [self.at_least_one(vars)] if param != 0 or param == len(vars) else []
+        clauses += [[x] for x in vars] if param == len(vars) else []
+        for combination in itertools.combinations([-x for x in vars[:]], param + 1):
             clause = []
-            for i in range(param+1):
+            for i in range(param + 1):
                 clause.append(combination[i])
             clauses.append(clause)
         return clauses
@@ -168,8 +184,13 @@ class Game:
         clauses = []
         pos = data["pos"]
         field = data["field"]
-        f = self.cell_to_variable(pos[0], pos[1], "W") if field == "sea" else -self.cell_to_variable(pos[0], pos[1], "W")
+        f = self.cell_to_variable(pos[0], pos[1], "W") if field == "sea" else -self.cell_to_variable(pos[0], pos[1],
+                                                                                                     "L")
         proximity_count = data.get("prox_count", None)
+        guess_animal = data.get("animal", None)
+        if guess_animal:
+            self.board[pos[0]][pos[1]][1] = guess_animal
+            clauses.append([self.cell_to_variable(pos[0], pos[1], guess_animal)])
         if proximity_count:
             self.board[pos[0]][pos[1]] = ["W" if field == "sea" else "L", 'F', proximity_count]
             clauses.append([self.cell_to_variable(pos[0], pos[1], 'F')])
@@ -177,7 +198,7 @@ class Game:
             for cell in near_cells:
                 if cell not in self.visitedCells:
                     self.visitedCells.append(cell)
-                    clauses += self.create_rule_on_cell(pos[0], pos[1])
+                    clauses += self.create_rule_on_cell(cell[0], cell[1])
             animals = ("T", "S", "C")
             total_count = 0
             for index, count in enumerate(proximity_count):
@@ -185,7 +206,6 @@ class Game:
                 cells = []
                 animal = animals[index]
                 for cell in near_cells:
-                    print(data, cell, near_cells, animal, self.cell_to_variable(cell[0], cell[1], animal))
                     cells.append(self.cell_to_variable(cell[0], cell[1], animal))
                 clauses += self.exact(cells, count)
             cells = []
@@ -193,6 +213,6 @@ class Game:
                 cells.append(self.cell_to_variable(cell[0], cell[1], "F"))
             clauses += self.exact(cells, len(near_cells) - total_count)
         else:
-            self.board[pos[0]][pos[1]] = ["W" if field == "sea" else "L", '?', []]
+            self.board[pos[0]][pos[1]][0] = "W" if field == "sea" else "L"
         clauses.append([f])
         self.clauses += clauses
--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 from crocomine.client.crocomine_client import CrocomineClient
 from Game import Game
 
+values_list = ["L", "W", "F", "T", "C", "S"]
+values_dict = {
+    "L": 1,
+    "W": 2,
+    "F": 3,
+    "T": 4,
+    "C": 5,
+    "S": 6,
+}
+
+def test():
+    m = 10
+    n = 10
+    game = Game(m, n, 0, 0, 0, 0, 0)
+    for i in range(m):
+        for j in range(n):
+            for key in values_dict:
+                var = game.cell_to_variable(i, j, key)
+                cell = game.variable_to_cell(var)
+                if i != cell[0] or j != cell[1] or key != cell[2]:
+                    print(var, cell, (i, j, key))
 
 def main():
     server = "http://localhost:8000"
@@ -34,32 +55,30 @@ def main():
             end = False
         elif status == 'GG' or status == 'KO':
             status, msg, grid_infos = mine.new_grid()
-            game = Game(
-                grid_infos['m'],
-                grid_infos['n'],
-                grid_infos['tiger_count'],
-                grid_infos['shark_count'],
-                grid_infos['croco_count'],
-                grid_infos['land_count'],
-                grid_infos['sea_count']
-            )
-            status, msg, infos = mine.discover(grid_infos['start'][0], grid_infos['start'][1])
+            if status != 'Err':
+                game = Game(
+                    grid_infos['m'],
+                    grid_infos['n'],
+                    grid_infos['tiger_count'],
+                    grid_infos['shark_count'],
+                    grid_infos['croco_count'],
+                    grid_infos['land_count'],
+                    grid_infos['sea_count']
+                )
+                status, msg, infos = mine.discover(grid_infos['start'][0], grid_infos['start'][1])
+            else:
+                end = False
         elif status == 'OK':
             for cell in infos:
                 game.add_information_constraints(cell)
-            game.make_decision()
-
-            status, msg, grid_infos = mine.new_grid()
-            game = Game(
-                grid_infos['m'],
-                grid_infos['n'],
-                grid_infos['tiger_count'],
-                grid_infos['shark_count'],
-                grid_infos['croco_count'],
-                grid_infos['land_count'],
-                grid_infos['sea_count']
-            )
-            status, msg, infos = mine.discover(grid_infos['start'][0], grid_infos['start'][1])
+            action, cell = game.make_decision()
+            print(action, cell)
+            if action == 'guess':
+                status, msg, infos = mine.guess(cell[0], cell[1], cell[2])
+            elif action == 'discover':
+                status, msg, infos = mine.discover(cell[0], cell[1])
+            else:
+                end = False
 
     print('Fin')
 
